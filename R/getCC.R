3949c0eb3ecf22d86f03b5fd1eb6fa1a3612080b
Yuhui Yao <bolus123@users.noreply.github.com>
2020-12-09 15:56:23 -0600

    Update getCC.R

--- R/getCC.R
+++ R/getCC.R
@@ -2,45 +2,27 @@
   #Matrix form
 ##############################################
 InvertQ <- function(coef){
+  
+  out <- 1
+  
+  if (all(abs(coef) < 1)) {
+    
+    minmod <- min(Mod(polyroot(c(1, coef))))
+    
+    if (minmod <= 1) {
+      
+      out <- 0
+      
+    }
+    
+  } else {
+    
+    out <- 0
+    
+  }
 
-      out <- 1
-
-      if (all(abs(coef) < 1)) {
-
-          stopifnot(class(coef)=="numeric"||class(coef)=="matrix"||(class(coef)=="array" && (dim(coef)[1]==dim(coef)[2])))
-          if (class(coef) == "numeric")
-            coef <- array(coef,dim=c(1,1,length(coef)))
-          if (class(coef) == "matrix")
-            coef <- array(coef,dim=c(NROW(coef),NROW(coef),1))
-          k <- dim(coef)[1]
-          order <- dim(coef)[3]
-          if (order==1)
-            ans <- eigen(coef[,,1], symmetric=FALSE, only.values =TRUE)$value
-          else{
-            blockMat <- matrix(numeric((k*order)^2),k*order,k*order)
-            blockMat[1:k,] <- coef
-            Imat <- diag((order-1)*k)
-            blockMat[((k+1):(k*order)),1:((order-1)*k)] <- Imat
-            ans <- eigen(blockMat, symmetric=FALSE, only.values =TRUE)$value
-          }
-          MaxEigenvalue <- max(Mod(ans))
-
-          if (MaxEigenvalue >= 1) {
-
-            out <- 0
-
-          }
-
-        } else {
-
-          out <- 0
-
-      }
-
-
-
-    return(out)
-
+  return(out)
+  
 }
 
 parsMat <- function(n, parsVec, norder = 1) {
